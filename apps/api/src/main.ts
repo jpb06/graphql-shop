e5c033c599cc03b5292ee788958fffa1cae513d6
jpb06 <jp.bois.06@outlook.fr>
2022-08-28 14:58:18 +0200

    adding auto validation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './modules/app.module';
@@ -12,6 +12,8 @@ const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
