8dc336a09ebf8eceaa97bd4b3eb1cdf2f4d43050
jpb06 <jp.bois.06@outlook.fr>
2022-09-19 19:02:32 +0200

    chore: fixing size definition for button

--- apps/front/src/components/generic/button/Button.tsx
+++ apps/front/src/components/generic/button/Button.tsx
@@ -5,8 +5,7 @@ type ButtonVariant = 'green' | 'neutral';
 type ButtonProps = {
   onClick: () => void;
   variant: ButtonVariant;
-  width: number;
-  height: number;
+  className: string;
 };
 
 const getVariantClasses = (variant: ButtonVariant) => {
@@ -14,15 +13,14 @@ const getVariantClasses = (variant: ButtonVariant) => {
     case 'green':
       return 'bg-green-600 hover:bg-green-700 focus:ring-green-800 text-white';
     case 'neutral':
-      return 'border border-gray-600 text-gray-400 hover:text-white focus:z-10 focus:ring-gray-700 bg-gray-700 ';
+      return 'border border-gray-600 text-gray-400 hover:text-white focus:z-10 focus:ring-gray-700 bg-gray-700';
   }
 };
 
 export const Button = ({
   onClick,
   variant,
-  width,
-  height,
+  className,
   children,
 }: PropsWithChildren<ButtonProps>) => {
   const variantStyles = getVariantClasses(variant);
@@ -30,7 +28,7 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={`mr-2  mb-2 rounded-lg px-5 py-2.5 text-sm font-medium focus:outline-none focus:ring-4 w-${width} h-${height} ${variantStyles} text-xl`}
+      className={`mr-2 mb-2 rounded-lg px-5 py-2.5 text-sm font-medium focus:outline-none focus:ring-4 ${variantStyles} ${className} text-xl`}
       onClick={onClick}
     >
       {children}
