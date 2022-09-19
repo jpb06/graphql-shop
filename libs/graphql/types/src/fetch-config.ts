fba8a9a56c5af943257a46f8daae397878a067a9
jpb06 <jp.bois.06@outlook.fr>
2022-09-19 20:11:43 +0200

    chore: bumping deps

--- libs/graphql/types/src/fetch-config.ts
+++ libs/graphql/types/src/fetch-config.ts
@@ -1,7 +1,3 @@
-//import { queryClient } from './../../../front/src/pages/_app';
-
-//queryClient.getQueryData("")
-
 export const endpointUrl = process.env.NEXT_PUBLIC_GQL_API_URL;
 export const fetchParams = {
   headers: {
