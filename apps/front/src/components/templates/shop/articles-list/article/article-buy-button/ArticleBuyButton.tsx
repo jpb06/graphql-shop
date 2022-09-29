d4cb2a3b12f24af60bac23291d2f8613fcaf988d
jpb06 <jp.bois.06@outlook.fr>
2022-09-29 19:10:00 +0200

    chore: out of stock disclaimer

--- apps/front/src/components/templates/shop/articles-list/article/article-buy-button/ArticleBuyButton.tsx
+++ apps/front/src/components/templates/shop/articles-list/article/article-buy-button/ArticleBuyButton.tsx
@@ -6,6 +6,14 @@ import { useOrderActions } from '../hooks/useOrderActions';
 export const ArticleBuyButton = (order: ArrayItemType<ProductsArrayType>) => {
   const { handleBumpOrder } = useOrderActions(order);
 
+  if (order.stock === 0) {
+    return (
+      <div className="cursor-pointer rounded-lg bg-rose-800 py-2 text-center text-xl text-gray-200">
+        Out of stock
+      </div>
+    );
+  }
+
   return (
     <button
       className="flex items-center justify-center rounded-lg bg-sky-800 py-6 px-3 text-xl text-white hover:bg-sky-700 sm:py-2"
