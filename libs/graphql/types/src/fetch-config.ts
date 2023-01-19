7c9725ebad8ea079cd83a7632c3f9b5e28cd2788
jpb06 <jp.bois.06@outlook.fr>
2023-01-19 21:04:11 +0100

    chore: switching to artifacts lib

--- libs/graphql/types/src/fetch-config.ts
+++
@@ -1,8 +0,0 @@
-export const endpointUrl =
-  process.env.NEXT_PUBLIC_GQL_API_URL || 'gql-api-url-not-set';
-
-export const fetchParams = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-};
